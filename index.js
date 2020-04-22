23e59eccd3ab519ce2b2f05420d87b92ec7bae3a
Linus Karsai <lkarsai@atlassian.com>
2020-04-22 13:39:19 +1000

    Show list of rolls

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const alfy = require("alfy");
 const ini = require("ini");
+const dayjs = require("dayjs");
+const relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime)
+
+const cachedRolls = alfy.cache.get("rolls") || [];
 
 const data = await alfy.fetch("https://rolz.org/api/?" + alfy.input, {
   json: false,
 });
 const result = ini.parse(data);
 
-alfy.output([{ 
-  title: `${result.result}`,
-  subtitle: `${result.input} = ${result.details}`,
-  icon: {
-    path: './dice.png'
-  } 
- }]);
+cachedRolls.unshift({
+  ...result,
+  time: dayjs(),
+});
+
+alfy.cache.set("rolls", cachedRolls, {maxAge: 600000});
+
+alfy.output(
+  cachedRolls.map((roll) => ({
+    title: `${roll.result}`,
+    subtitle: `🎲 ${roll.input} | 🧮 ${roll.details} | ⏲ ${dayjs(roll.time).fromNow()}`,
+    time: dayjs(),
+    icon: {
+      path: "./dice.png",
+    },
+  }))
+);
+
